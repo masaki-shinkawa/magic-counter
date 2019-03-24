9119c3327be7e21935ee26162bd1ddf6987721eb
masaki-shinkawa <masaki.shinkawa0303@gmail.com>
2019-03-24 20:27:52 +0900

    pwa

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import store from './store';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
+import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
