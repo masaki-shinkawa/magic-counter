a5cb391c4fc04438f56bc5ec2b2f87ab3862f9ed
Masaki Shinkawa <masaki.shinkawa@kdl.co.jp>
2019-03-25 12:01:04 +0900

    add ga

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import VueMaterial from "vue-material";
+import VueAnalytics from "vue-analytics";
 import "vue-material/dist/vue-material.min.css";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueMaterial);
+Vue.use(VueAnalytics, { id: "UA-136786420-1", router });
 
 new Vue({
   router,
