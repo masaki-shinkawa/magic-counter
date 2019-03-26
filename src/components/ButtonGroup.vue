8ccd912ed47748d31f624ab537f2e1c3422ab709
masaki-shinkawa <masaki.shinkawa0303@gmail.com>
2019-03-27 08:52:10 +0900

    使い方ボタンの追加

--- src/components/ButtonGroup.vue
+++ src/components/ButtonGroup.vue
@@ -1,18 +1,31 @@
 <template>
-  <v-layout align-center>
-    <v-flex xs12 sm12 text-xs-center>
-      <v-btn small color="error" @click="allClick">全てクリア</v-btn>
-    </v-flex>
-  </v-layout>
+  <v-container fluid class="pa-0">
+    <v-layout align-center>
+      <v-flex xs6 sm4 text-xs-center>
+        <v-btn large color="error" @click="allClick">全てクリア</v-btn>
+      </v-flex>
+      <v-flex xs6 sm4 text-xs-center>
+        <v-btn large color="private" @click="openUsage">使い方</v-btn>
+      </v-flex>
+    </v-layout>
+    <counter-usage-dialog v-model="isOpen" />
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: "buttonGroup",
-  methods: {
-    async allClick() {
-      await this.$store.dispatch("allClearCounter", this.index);
-    }
+import { Vue, Component } from "vue-property-decorator";
+import CounterUsageDialog from "@/components/dialog/CounterUsageDialog";
+
+@Component({ components: { CounterUsageDialog } })
+export default class ButtonGroup extends Vue {
+  isOpen = false;
+
+  async allClick() {
+    await this.$store.dispatch("allClearCounter", this.index);
   }
-};
+
+  openUsage() {
+    this.isOpen = true;
+  }
+}
 </script>
---
+++ src/components/dialog/CounterUsageDialog.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="text-xs-center">
+    <v-dialog v-model="localValue" width="500">
+      <v-card>
+        <v-card-title class="headline grey lighten-2" primary-title>
+          使い方
+        </v-card-title>
+
+        <v-card-text>
+          TODO: 使い方を書く
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" flat @click="close">
+            閉じる
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import { Vue, Component, Emit } from "vue-property-decorator";
+@Component({
+  props: {
+    value: {
+      type: Boolean
+    }
+  }
+})
+export default class CounterUsageDialog extends Vue {
+  @Emit()
+  input() {}
+
+  get localValue() {
+    return this.value;
+  }
+
+  set localValue(value) {
+    this.input(value);
+  }
+
+  close() {
+    this.input(false);
+  }
+}
+</script>
